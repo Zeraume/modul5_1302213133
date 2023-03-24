a7d76ae5d176638dd10a4a78daad730f07f46634
Faris Siddiq <farissiddiqrp@gmail.com>
2023-03-24 08:34:28 +0700

    menambahkan class Penjumlahan dengan method JumlahTigaAngka

---
+++ modul5_1302213133/Penjumlahan.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul5_1302213133
+{
+    public class Penjumlahan
+    {
+
+        public static T JumlahTigaAngka<T>(T input1, T input2, T input3)
+        {
+            dynamic hasil = null;
+            hasil = (dynamic)input1 + (dynamic)input2 + (dynamic)input3;
+            return hasil;
+        }
+    }
+}
